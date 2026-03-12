6cdcf5fddf8334aaa7be221fdb92db7e011c0d4e
shun <shun.soccer.shun@gmail.com>
2026-03-12 10:45:43 +0900

    test削除

--- src/main/java/com/example/taskManager/repository/TestUserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.taskManager.repository;
-
-import com.example.taskManager.entity.TestTable;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-// entityをDBに保存したり取得する窓口
-public interface TestUserRepository extends JpaRepository<TestTable, Integer> {
-}
